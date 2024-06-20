1349354874b225648a57dd1b96dfd4dc9ad28f61
Jordan Huizenga <jordan.huizenga@readytech.io>
2024-06-20 02:40:02 +0000

    add in some more options

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-# Mermaid Gantt Generator
-This program generates a [mermaid.js compatible gantt chart](https://mermaid.js.org/syntax/gantt.html). 
+# Gantt Generator
+This program generates a gantt chart based on a number of items in priority order and how long they would be expected to take.
+
+# Output Formats
+This program supports a number of output options, including:
+- [mermaid.js compatible gantt chart](https://mermaid.js.org/syntax/gantt.html). 
+- Markdown table
+- Plain text text
+- "Confluence compatible table" (you can copy/paste the output into confluence and it will render as a table)
 
 # Usage
 
@@ -10,10 +17,22 @@ For a list of different options use the `--help` command line flag.
 ```
 ruby generate-gantt.rb --help
 
-Usage: generate-gantt.rb -f CSV_FILE [-s START_DATE] [-m MAX_WORK_IN_PROGRESS]
+Usage: generate-gantt.rb -f CSV_FILE -o FORMAT [-s START_DATE] [-m MAX_WORK_IN_PROGRESS] [-m MULTIPLIER]
     -f, --file CSV_FILE              The CSV file containing the tasks
     -s, --start_date [STRING]        The start date of the project
-    -m [INTEGER],                    The maximum number of tasks that can be in progress at the same time
-        --max_work_in_progress
+        --max_work_in_progress [INTEGER]
+                                     The maximum number of tasks that can be in progress at the same time
+    -m, --multiplier [FLOAT]         The multiplier to the duration
+    -o, --output [FORMAT]            The output format, one of (g|gantt|mermaid), (s|string), (c|confluence) or (m|markdown)
     -h, --help                       Prints this help
+```
+
+Example CSV File would look something like this
+
+```csv
+Item,Min Duration,Max Duration
+Login,4,4
+Place Order,6,6
+Returns,26,26
+Invoicing,12,12
 ```
--- generate-gantt.rb
+++ generate-gantt.rb
@@ -19,6 +19,51 @@ GanttTask = Data.define(:name, :start_date, :end_date) do
   end
 end
 
+class SimpleFormatter
+  def format(tasks)
+    tasks.map do |task|
+      "Task: #{task.name}, Start: #{task.start_date}, End: #{task.end_date}, Duration: #{task.duration}"
+    end 
+  end
+end
+
+class MermaidGanttFormatter
+  def format(tasks)
+    output = "gantt\n    title Project Timeline\n    dateFormat  YYYY-MM-DD\n    axisFormat  %d-%m-%Y\n\n"
+    output += tasks.map { |task| "    #{task.format_to_mermaid}" }.join("\n")
+    output  
+  end
+end
+
+class ConfluenceTableFormatter
+  def format(tasks)
+    table = "|Item|Start|Time Taken|End|\n"
+    table << "|:------|------:|------:|-----:|\n"
+    tasks.each do |task|
+      table << "|#{task.name}|#{format_date(task.start_date)}|#{task.duration}|#{format_date(task.end_date)}|\n"
+    end
+    table
+  end
+
+  private
+
+  def format_date(date)
+    # print date in the format: MMM DD, YYYY, eg. Jul 01, 2025
+    date.strftime("%b %d, %Y")
+  end
+end
+
+class MarkdownTableFormatter
+  def format(tasks)
+    table = "| Item | Start | Time Taken | End |\n"
+    table << "|------|-------|------------|-----|\n"
+    tasks.each do |task|
+      table << "| #{task.name} | #{task.start_date} | #{task.duration} | #{task.end_date} |\n"
+    end
+    table
+  end
+end
+
 # Takes an array of tasks and returns an array of GanttTask objects
 # @param tasks [Array<Task>]
 # @param start_date [Date] The start date of the project
@@ -44,6 +89,19 @@ def tasks_to_gantt(tasks, start_date, max_work_in_progress)
   gantt_tasks
 end
 
+# Takes an array of Tasks and applies the given multiplier to the duration.
+# Multiplier is a float value that will be multiplied by the duration of each task.
+# For example, if the duration was 10, and multiplier 1.3, the new duration will be 13.
+# It always rounds up.
+# @param tasks [Array<Task>]
+# @param multiplier [Float]
+# @return [Array<Task>]
+def apply_multiplier(tasks, multiplier)
+  tasks.map do |task|
+    Task.new(name: task.name, duration: (task.duration * multiplier).ceil)
+  end
+end
+
 def csv_to_tasks(csv_file)
   tasks = CSV.read(csv_file, headers: true)
   tasks.map do |row|
@@ -53,17 +111,28 @@ def csv_to_tasks(csv_file)
   end
 end
 
-def generate_mermaid_gantt(csv_file, start_date, max_work_in_progress)
-  tasks = csv_to_tasks(csv_file)
+def generate_output(csv_file, start_date, max_work_in_progress, multiplier, formatter)
+  tasks = apply_multiplier(csv_to_tasks(csv_file), multiplier)
   gantt_tasks = tasks_to_gantt(tasks, start_date, max_work_in_progress)
 
-  gantt_chart = "gantt\n    title Project Timeline\n    dateFormat  YYYY-MM-DD\n    axisFormat  %d-%m-%Y\n\n"
-  gantt_chart + gantt_tasks.map { |task| "    #{task.format_to_mermaid}" }.join("\n")
+  formatter.format(gantt_tasks)
+end
+
+def output_to_mermaid(gantt_tasks)
+  output = "gantt\n    title Project Timeline\n    dateFormat  YYYY-MM-DD\n    axisFormat  %d-%m-%Y\n\n"
+  output += gantt_tasks.map { |task| "    #{task.format_to_mermaid}" }.join("\n")
+  output
+end
+
+def output_to_string(gantt_tasks)
+  gantt_tasks.map do |task|
+    "Task: #{task.name}, Start: #{task.start_date}, End: #{task.end_date}, Duration: #{task.duration}"
+  end
 end
 
 options = {}
 OptionParser.new do |opts|
-  opts.banner = "Usage: generator.rb -f CSV_FILE [-s START_DATE] [-m MAX_WORK_IN_PROGRESS]"
+  opts.banner = "Usage: generate-gantt.rb -f CSV_FILE -o FORMAT [-s START_DATE] [-m MAX_WORK_IN_PROGRESS] [-m MULTIPLIER]"
 
   opts.on("-f", "--file CSV_FILE", "The CSV file containing the tasks") do |csv_file|
     options[:csv_file] = csv_file
@@ -75,6 +144,28 @@ OptionParser.new do |opts|
 
   opts.on("-m", "--max_work_in_progress [INTEGER]", Integer, "The maximum number of tasks that can be in progress at the same time") do |max_work_in_progress|
     options[:max_work_in_progress] = max_work_in_progress
+  end
+
+  # add option to add multiplier to the duration
+  opts.on("-m", "--multiplier [FLOAT]", Float, "The multiplier to the duration") do |multiplier|
+    options[:multiplier] = multiplier
+  end
+
+  opts.on("-o", "--output [FORMAT]", String, "The output format, one of (g|gantt|mermaid), (s|string), (c|confluence) or (m|markdown)") do |output|
+    case output
+    when "g", "mermaid", "gantt"
+      options[:formatter] = MermaidGanttFormatter.new
+    when "s", "string"
+      options[:formatter] = SimpleFormatter.new
+    when "c", "confluence"
+      options[:formatter] = ConfluenceTableFormatter.new
+    when "m", "markdown"
+      options[:formatter] = MarkdownTableFormatter.new
+    else
+      raise "Invalid output format: #{output}"
+      puts opts
+      exit
+    end
   end
 
   opts.on("-h", "--help", "Prints this help") do
@@ -90,8 +181,19 @@ OptionParser.new do |opts|
     exit
   end
 
+  unless File.exist?(options[:csv_file])
+    puts "Error: File not found: #{options[:csv_file]}"
+    exit
+  end
+  
+  unless options[:formatter]
+    puts "Error: Missing output format option"
+    puts opts
+    exit
+  end
+
   begin
-    puts generate_mermaid_gantt(options[:csv_file], options[:start_date] || Time.zone.today, options[:max_work_in_progress] || 2)
+    puts generate_output(options[:csv_file], options[:start_date] || Date.today, options[:max_work_in_progress] || 2, options[:multiplier], options[:formatter])
   rescue => e
     puts "Error: #{e.message}"
     exit
